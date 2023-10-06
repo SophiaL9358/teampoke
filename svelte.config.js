3df057b66de303e77b9f20c2b7a8d091d4a259db
soapity <mrows9356@gmail.com>
2023-10-06 15:13:12 -0400

    forrender

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,12 @@
-import adapter from '@sveltejs/adapter-netlify';
+import node from '@sveltejs/adapter-node';
 
-
-export default {
+const config = {
 	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: node()
 	}
 };
+
+export default config;
