67aa60f14e386e093a99cccabd11b9f68f274fe8
soapity <mrows9356@gmail.com>
2023-10-25 14:30:31 -0400

    login + design done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Footer from "./Footer.svelte";
+	import GoogleAuthButton from "./GoogleAuthButton.svelte";
+
+
+</script>
+
+<section class = "w-100 bg-light-yellow flex-center flex-column py-5" style = "height: 80vh;">
+    
+    <h1>Mock Interview Practice</h1>
+    <br>
+    <br>
+    <GoogleAuthButton />
+    <button class = "poke-btn bg-light-green">
+        Contine As Guest
+    </button>
+    
+</section>
+<Footer/>
+<style>
+
+</style>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,10 @@
+<section class = "flex-center p-4 w-100 bg-light-yellow "  style = "height: 20vh;">
+    <img src = "https://cdn.firespring.com/images/5adbf42d-0a16-4ec5-ae1b-0ec931f8247a.png" alt = "Arc of Frederick County Logo"/>
+    <span class = "ms-auto">Questions? Contact <a href= "mailto:poke.co2023@gmail.com">poke.co2023.gmail.com</a></span>
+</section>
+
+<style>
+    img {
+        height: 120px;
+    }
+</style>
