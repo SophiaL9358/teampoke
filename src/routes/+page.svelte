82dd3bb8deaeccb153403ad3a0d21004c74c4e63
soapity <mrows9356@gmail.com>
2023-10-26 14:25:58 -0400

    home updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <a target ="_blank" href = "https://www.arcfc.org/">
     <img src = "https://cdn.firespring.com/images/5adbf42d-0a16-4ec5-ae1b-0ec931f8247a.png" id = "logo" alt = "Arc of Frederick County Logo"/>
 </a>
-<section class = "w-100 bg-light-yellow flex-center flex-column w-100 text-center" style = "height: 100vh;">
+<section class = "bg-light-yellow flex-center flex-column text-center" style = "height: 100vh;">
     <h1>Mock Interview Practice</h1>
     <span class = "mb-5">Questions? Email <a href="mailto:poke.co2023@gmail.com">poke.co2023@gmail.com</a></span>
     
@@ -23,13 +23,13 @@
 <style>
     #logo {
         position: absolute;
-        height: 150px;
+        height: 130px;
         bottom: 20px;
         left: 20px;
         
     }
     section {
-        padding: 100px 0px 100px 0px !important;
+        padding: 100px 25px 100px 25px !important;
 
     }
 </style>
