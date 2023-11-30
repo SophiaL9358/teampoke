9e0088dffd0f523604f1087738f9b6f0fd679f2d
ASaridena27 <135557472+ASaridena27@users.noreply.github.com>
2023-11-30 19:30:20 +0000

    new page poggers

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<div class = 'w-100' style = "height: 100vh;">
+      <button class = "btn bg-light-green" style = "height: 500; width: 200px; "> Start Interview </button>
+      <button class = "btn bg-light-green" style = "height: 500; width: 200px; "> Past Interviews </button>
+      <button class = "btn bg-light-green" style = "height: 500; width: 200px; "> Update Resume </button>
+</div>
