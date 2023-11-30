aa2e0dc38dca4f3136f36e88285c6543439374cf
jocel-4 <75513212+jocel-4@users.noreply.github.com>
2023-11-30 19:07:16 +0000

    resume changes

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    export let data;
+    import { initializeApp } from "firebase/app";
+    import { getFirestore } from "firebase/firestore";
+   
+
+
+    let files;
+    let filename;
+
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyDIMSd2qMl2vltWtTDgNjRFlgTWl73jMXw",
+        authDomain: "mock-interview-app-403217.firebaseapp.com",
+        projectId: "mock-interview-app-403217",
+        storageBucket: "mock-interview-app-403217.appspot.com",
+        messagingSenderId: "1083819190018",
+        appId: "1:1083819190018:web:046cef3647a6736bd0841b",
+        measurementId: "G-K0RYJF8CBX"
+    };
+
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+   
+    const onChange = () => {
+        if (files && files[0]) {
+            filename = files[0].name;
+            console.log(filename);
+        } else {
+            console.log("Didn't work");
+        }
+    }
+
+
+</script>
+
+
+
+
+
+<style>
+
+
+</style>
+
+
+<body class="bg-light-yellow">
+   
+    <section class = "bg-light-yellow flex-center flex-column text-center" style = "height: 100vh;">
+
+
+        <!-- https://gist.github.com/dahnielson/59566b3d91cc8cf09c13d1419604a8a2 -->
+        <div class = "">
+            <label for="files" class="text-center w-100 upload-btn bg-light-green">Upload Resume</label>
+            <input id="files" style="visibility:hidden;" type="file" bind:files accept="application/pdf,application/msword,
+            application/vnd.openxmlformats-officedocument.wordprocessingml.document">
+          </div>
+            {#if files && files[0]}
+                <p>
+                    {files[0].name}
+                </p>
+            {/if}
+    </section>
+
+
+</body>
